82b33c13a0a1376c2650ceb95085541fc517636f
Abir Abdullah <abirabdullah3491@gmail.com>
2025-11-04 08:47:00 +0600

    build

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './Header';
 import { useLoaderData } from 'react-router';
-import { CgLaptop } from 'react-icons/cg';
 import Apps from './Apps';
 
 const Home = () => {
