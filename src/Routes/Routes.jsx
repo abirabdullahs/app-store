73c696e2cf29667c98209d4986e235e2cc98c49f
Abir Abdullah <abirabdullah3491@gmail.com>
2025-11-04 14:07:26 +0600

    ok

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
 import Root from '../Root/Root';
 import ErrorPage from '../pages/ErrorPage';
 import Home from '../pages/Home/Home';
@@ -8,37 +7,31 @@ import AppDetailes from '../pages/AppDtailes/AppDetailes';
 import AllApps from '../pages/AllApps/AllApps';
 import Installation from '../pages/Installation/Installation';
 
-
-
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: Root,
-    errorElement: <ErrorPage></ErrorPage>,
-    children : [
+    errorElement: <ErrorPage />,
+    children: [
       {
         index: true,
-       
-        loader: () => fetch('apps.json').then(res => res.json()),
-        path: '/',
-        Component: Home
+        loader: () => fetch('/apps.json').then(res => res.json()),
+        Component: Home,
       },
       {
-      
-        loader: () => fetch('apps.json').then(res => res.json()),
         path: '/Apps',
-        Component: AllApps
+        loader: () => fetch('/apps.json').then(res => res.json()),
+        Component: AllApps,
       },
       {
-              path: '/Installation',
-              Component: Installation
+        path: '/Installation',
+        Component: Installation,
       },
       {
-        loader: ()=> fetch('apps.json').then(res => res.json()),
         path: '/Apps/:id',
-        Component: AppDetailes
-      }
-      
-    ]
+        loader: () => fetch('/apps.json').then(res => res.json()),
+        Component: AppDetailes,
+      },
+    ],
   },
 ]);
